d4a1edbd4c4e1dea9e4e90f13a9a0e043e3ae1fd
Albany276 <albany.alfonzo@hotmail.com>
2020-11-21 09:59:20 +1000

    updated products model
    updated with fields to match spec, made a lot of the fields as optional. tested creating a new product, it works  but when entering the screen size you need to write inches rather than using "

--- groupProjectBackend/products/models.py
+++ groupProjectBackend/products/models.py
@@ -5,13 +5,21 @@ from django.db import models
 class Product(models.Model):
     category = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
+    os = models.TextField(default="")
+    model_tech =  models.TextField(default="")
     spec1 = models.TextField()
     spec2 = models.TextField()
     spec3 = models.TextField()
     spec4 = models.TextField()
     spec5 = models.TextField()
+    spec6 = models.TextField(default="")
     image = models.URLField()
     price = models.IntegerField()
+    justification = models.TextField(default="")
     supplier1 = models.URLField()
     supplier2 = models.URLField()
+    overview = models.TextField(default="")
+
+
+    
 
--- groupProjectBackend/products/serializers.py
+++ groupProjectBackend/products/serializers.py
@@ -4,17 +4,24 @@ from .models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
+
+
     category = serializers.CharField(max_length=50)
     brand = serializers.CharField(max_length=50)
+    os = serializers.CharField(required=False, allow_blank=True, max_length=None)
+    model_tech = serializers.CharField(required=False, allow_blank=True, max_length=None)
     spec1 = serializers.CharField(required=False, allow_blank=True, max_length=None)
     spec2 = serializers.CharField(required=False, allow_blank=True, max_length=None)
     spec3 = serializers.CharField(required=False, allow_blank=True, max_length=None)
     spec4 = serializers.CharField(required=False, allow_blank=True, max_length=None)
     spec5 = serializers.CharField(required=False, allow_blank=True, max_length=None)
+    spec6 = serializers.CharField(required=False, allow_blank=True, max_length=None)
     image = serializers.URLField()
     price = serializers.IntegerField()
-    supplier1 = serializers.URLField()
-    supplier2 = serializers.URLField()
+    justification = serializers.CharField(required=False, allow_blank=True, max_length=None)
+    supplier1 = serializers.URLField(required=False, allow_blank=True)
+    supplier2 = serializers.URLField(required=False, allow_blank=True)
+    overview = serializers.CharField(required=False, allow_blank=True, max_length=None)
 
 
     def create(self, validated_data):
